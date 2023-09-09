205a6ee947d73d399b45079068248c8e1c5bdbd5
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-09-09 07:52:40 +0200

    Create url-streams.js

---
+++ src/modules/streams/transform/url-streams.js
@@ -0,0 +1,10 @@
+// import * as urlStreams from "./url-streams.js"
+
+// new TransformStream(urlStreams.fetch)
+export const fetch = {transform(fetchArguments,controller){
+  globalThis.fetch(fetchArguments).then(controller.enqueue);
+}};
+  
+export const reFetchIfPossible = transform(response,controller){
+  data.statusCode.startsWith(3) && fetch(response.url)
+}};
